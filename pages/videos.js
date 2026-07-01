735a906dd13be7f455827bd15d8a3ec9efa54750
ddoddofam <hgkim.mobile@gmail.com>
2026-07-01 16:13:03 +0900

    Add files via upload

---
+++ pages/videos.js
@@ -0,0 +1,15 @@
+// 1. 국어 영상 데이터
+const koreanVideos = [
+    { title: "1화 아이야", link: "https://youtu.be/sjxbhMNV97M?si=rmk-PFprTEUijHDo", desc: "모음 'ㅏ','ㅣ','ㅑ'의 소리를 인식한다." },
+    { title: "9화 나비", link: "https://youtu.be/1DA6tueYG3s?si=I-j5a-ZgAilrGgIZ", desc: "자음 'ㄴ' 의 소리를 인식한다." }
+];
+
+// 2. 수학 영상 데이터
+const mathVideos = [
+    { title: "넘버블록스 11-20 모음", link: "https://www.youtube.com/watch?v=..." }
+];
+
+// 3. 영어 영상 데이터
+const englishVideos = [
+    { title: "Hello Song", link: "https://www.youtube.com/watch?v=..." }
+];
