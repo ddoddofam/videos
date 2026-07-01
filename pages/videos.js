f4e4677af938407c208263f1dc6d233e78082864
ddoddofam <hgkim.mobile@gmail.com>
2026-07-01 16:58:55 +0900

    Add files via upload

--- pages/videos.js
+++ pages/videos.js
@@ -1,7 +1,20 @@
 // 1. 국어 영상 데이터
 const koreanVideos = [
     { title: "1화 아이야", link: "https://youtu.be/sjxbhMNV97M?si=rmk-PFprTEUijHDo", desc: "모음 'ㅏ','ㅣ','ㅑ'의 소리를 인식한다." },
-    { title: "9화 나비", link: "https://youtu.be/1DA6tueYG3s?si=I-j5a-ZgAilrGgIZ", desc: "자음 'ㄴ' 의 소리를 인식한다." }
+    { title: "2화 오이", link: "https://youtu.be/labPzGmG0jw?si=v6CxCPRIddspqbty", desc: "모음 'ㅗ', 'ㅣ' 의 소리를 인식한다." },
+    { title: "3화 어디", link: "https://youtu.be/k_eCbbmXXmc?si=4bMphfqvTBKmwX8N", desc: "모음 'ㅓ', 'ㅣ'의 소리를 인식한다." },
+    { title: "4화 여우", link: "https://youtu.be/C4tBDGOA34M?si=OQWZKlW7pUjtNbgZ", desc: "모음 'ㅕ', 'ㅜ' 의 소리를 인식한다." },
+    { title: "5화 우유", link: "https://youtu.be/m1G8Wz3hxco?si=puSU9FhuEoOF0ZhK", desc: "모음 'ㅜ','ㅠ'의 소리를 인식한다." },
+    { title: "6화 요가", link: "https://youtu.be/RgqOMeXTZQ0?si=5K3uGOs6xXDi0th8", desc: "모음 'ㅗ','ㅏ'의 소리를 인식한다." },
+    { title: "7화 으", link: "https://youtu.be/cHS61BdT98I?si=Br1X_fGpq6nCmq4H", desc: "모음 'ㅡ'의 소리를 인식한다." },
+    { title: "8화 가수", link: "https://youtu.be/kANDJoLtgbQ?si=tvP7YEe_rmquFMai", desc: "자음 'ㄱ'의 소리를 인식한다." },
+    { title: "9화 나비", link: "https://youtu.be/1DA6tueYG3s?si=I-j5a-ZgAilrGgIZ", desc: "자음 'ㄴ'의 소리를 인식한다." },
+    { title: "10화 두유", link: "https://youtu.be/SYbQL7SOyxs?si=FBulquzb6JKjRZhK", desc: "자음 'ㄷ'의 소리를 인식한다" },
+    { title: "11화 라디오", link: "https://youtu.be/5XpgVOqSRFA?si=3HM51XcUIslvhI4y", desc: "자음 'ㄹ'의 소리를 인식한다." },
+    { title: "12화 모자", link: "https://youtu.be/ovZ0A3u56ts?si=0IaJjLvKjLdrG3xB", desc: "자음 'ㅁ'의 소리를 인식한다." },
+    { title: "13화 버스", link: "https://youtu.be/7ykvLdRFNWQ?si=FYAn_6n-MJ7BKXFz", desc: "자음 'ㅂ'의 소리를 인식한다." },
+    { title: "14화 소리", link: "https://youtu.be/zPPAN1bp0DA?si=qQzUWlr8Ub9URzGG", desc: "자음 'ㅅ'의 소리를 인식한다." },
+    { title: "15화 자유", link: "https://youtu.be/7JJup1IZETQ?si=58cYGiSykyIufxU4", desc: "자음 'ㅈ'의 소리를 인식한다." }
 ];
 
 // 2. 수학 영상 데이터
